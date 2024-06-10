3f1c015b7a949dbebfaefd35986209da1c1bb1e5
akash-bt21cse024 <146684897+akash-bt21cse024@users.noreply.github.com>
2024-06-10 06:56:01 +0000

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-const connectdb = require('./utils/Connectdb');
+const connectDb = require('./utils/connectdb');
 const cors = require('cors');
 const{signup,login}= require('./utils/user.util');
 
@@ -10,7 +10,7 @@ const {gamepost,getallgame}=require('./utils/game.util');
 app.use(express.json());
 app.use(cors());
 
-connectdb();
+connectDb();
 app.post('/api/auth/signup',signup)
 app.post('/api/auth/login',login)
 app.post('/',gamepost)
